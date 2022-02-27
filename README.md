a3c2d18012df774e12f40876a30528433d617647
melcherj22 <melcherj@oregonstate.edu>
2022-02-26 23:35:51 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,28 @@
+This is a microservice for scraping logos and images off of Wikipedia.
+
+It communicates with other programs through the 'signal.txt' file that is located in this directory.
+
+If you wish to make a request, write the following to the 'signal.txt' file:
+the word "image" or "logo", then the URL of the Wikipedia page that this image or logo is featured on.
+
+EXAMPLE REQUESTS:
+
+    image https://en.wikipedia.org/wiki/Oregon_State_University
+    
+    logo https://en.wikipedia.org/wiki/Apple_Inc.
+
+Requests will generate an 'output.png' file with the scraped image or logo. This 'output.png' file will be erased upon the next request, 
+so be sure to save it if you wish to use it for long-term purposes.
+
+The request made in the 'signal.txt' file will also be erased after it has been read by the 'wiki_scraper.py' file, so your program
+only needs to write to 'signal.txt'; it does not have to erase past requests before writing new ones.
+
+If no image is found on the given Wikipedia page, the 'wiki_scraper.py' file will print out "Image not found." It is still able to
+take new requests after this occurs.
+
+NOTES:
+    - A request for an image is less specific than a request for a logo, so sometimes scraped images are also logos.
+    - If you encounter an error that says "certificate verify failed: unable to get local issuer certificate", you likely need to install
+      the "Install Certificates.command" file located in your Python download directory for this microservice to operate correctly. This 
+      is done by simply double clicking the "Install Certificates.command" file. More information on this can be found here: 
+      https://stackoverflow.com/questions/52805115/certificate-verify-failed-unable-to-get-local-issuer-certificate.
---
+++ wiki_scraper.py
@@ -0,0 +1,77 @@
+# Author: Julia Melchert
+# Date: 2/22/22
+# Description: A microservice that scrapes Wikipedia for company logos or general images, given a Wikipedia page's URL.
+
+import time, requests, os, urllib.request, re
+from bs4 import BeautifulSoup
+
+# Runs infinitely in the background while waiting for a request to come in
+while True:
+
+    # Checks for a new request every second
+    time.sleep(1)
+
+    # Checks if any changes have been made to the signal.txt file (i.e. if a request has been made)
+    with open('signal.txt', 'r') as read_infile:
+        # Clears the signal.txt file after reading its contents
+        request = read_infile.read()
+        with open('signal.txt', 'w') as write_infile:
+            write_infile.write("")
+        
+        # If a request has come in:
+        if request != "":
+
+            # Removes any current output files that were created in past requests
+            if os.path.exists('output.png') is True:
+                os.remove('output.png')
+
+            # Splits the request type and URL into two separate list items (ex. ["logo", www.example.com])
+            request = request.split()
+
+            page = requests.get(request[1])
+            soup = BeautifulSoup(page.content, "html.parser")
+
+            try:
+
+                # If the request is for a logo:
+                if request[0] == "logo":
+                    # Finds the appropriate HTML and converts it into an image address
+                    logo = soup.find("table", class_="infobox vcard")
+
+                    # Ensures that the BeautifulSoup object does not turn up as "None" throughout its search
+                    if logo is not None:
+                        logo = soup.find("table", class_="infobox vcard").find("td", class_="infobox-image logo")
+
+                    if logo is not None:
+                        logo = soup.find("table", class_="infobox vcard").find("td", class_="infobox-image logo").find("img")
+
+                    if logo is not None:
+                        # After locating the logo, its image address is generated
+                        image_address = "https:" + logo['src']
+                        
+                        # Write image to output.png using the image address created above
+                        urllib.request.urlretrieve(image_address, 'output.png')
+
+                # If the request is for an image:
+                elif request[0] == "image":                
+                    # Finds the appropriate HTML and converts it into an image address
+                    # Finds the first HTML element with a class name that has the word "infobox" in it
+                    image = soup.find("table", **{'class' : re.compile('.*infobox.*')})
+
+                    # If there is an infobox, then looks for an image in the infobox
+                    if image is not None:
+                        image = soup.find("table", **{'class' : re.compile('.*infobox.*')}).find("img")
+
+                    # If there was an image in the infobox, then the image address is generated and a file (output.png) is created
+                    # with the image.
+                    if image is not None:
+                        image_address = "https:" + image['src']
+                    
+                        # Write image to output.png using the image address created above
+                        urllib.request.urlretrieve(image_address, 'output.png')
+
+            finally:
+
+                # If no image was found, then "Image not found" is printed out.
+                if os.path.exists('output.png') is False:
+                    print("Image not found.")
